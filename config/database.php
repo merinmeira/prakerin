efe06a941b6cdc8798d505dc6f6cdae068387dd4
merinmeira <merinmoy4@gmail.com>
2019-07-06 09:20:08 +0700

    tambah template dan index

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'pkl_merin'),
             'username' => env('DB_USERNAME', 'merinmeira'),
-            'password' => env('DB_PASSWORD', 'merin123@'),
+            'password' => env('DB_PASSWORD', 'merincantik_123@'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
